d9b9202883860e2cf82b345abcca2c9e592027f5
PCBL <pedro_castelo@hotmail.com>
2016-10-05 20:26:32 -0300

    First Commit

---
+++ SimpleCalculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    /// <summary>
+    /// Simple Calculator, built for GFT´s Architecture Journal 2nd Edition
+    /// </summary>
+    public class Calculator
+    {
+        /// <summary>
+        /// Sums up 2 numbers
+        /// </summary>
+        /// <param name="a">First Sumber</param>
+        /// <param name="b">Second Number</param>
+        /// <returns>Returns the result of the addition</returns>
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Substracts 2 numbers
+        /// </summary>
+        /// <param name="a">First Sumber</param>
+        /// <param name="b">Second Number</param>
+        /// <returns>Returns the result of the substraction</returns>
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ SimpleCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculatorTests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void When_Sum_1_and_2_expect_3()
+        {
+            const double a = 1;
+            const double b = 2;
+            const double expected = 3;
+            var calc = new Calculator();
+            var result = calc.Sum(a, b);
+            Assert.AreEqual(expected,result);
+        }
+
+        [TestMethod()]
+        public void When_Substract_5_and_2_expect_3()
+        {
+            const double a = 5;
+            const double b = 2;
+            const double expected = 3;
+            var calc = new Calculator();
+            var result = calc.Substract(a, b);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
